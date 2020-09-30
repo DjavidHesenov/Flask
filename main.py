1e9d61c39da5e4220e3bd8d0e2900ddfccacfff7
Djavid <hesenovdjavid@gmail.com>
2020-09-30 20:09:32 +0400

    push_2

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
-    return "About page"
+    return render_template("about.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page" + " " + name + " " + str(id)
 
 
 if __name__ == "__main__":
