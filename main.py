f4cad98e696a16a026721308fa8d9b5883aff203
Djavid <hesenovdjavid@gmail.com>
2020-10-02 20:53:39 +0400

    push_4

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
 
 @app.route('/')
 def index():
@@ -12,9 +31,24 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page" + " " + name + " " + str(id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+         title = request.form['title']
+         intro = request.form['intro']
+         content = request.form['content']
+
+         article = Article(title=title, intro=intro, content=content)
+
+         try:
+             db.session.add(article)
+             db.session.commit()
+             return redirect('/')
+         except:
+             return "Error occured"
+
+    else:
+        return render_template("create-article.html")
 
 
 if __name__ == "__main__":
